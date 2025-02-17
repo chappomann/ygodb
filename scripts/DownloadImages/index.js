b4d876fb4508cf0f63ac7e8afe97ab70eed51406
chappomann <lasanamilton@icloud.com>
2025-02-17 16:19:47 -0500

    backup and update to download large images

--- scripts/DownloadImages/index.js
+++ scripts/DownloadImages/index.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises');
 const { findImgs, downloadImages } = require('./helpers/generateImgArray');
 
 async function main() {
-    const imagesToDownload = await findImgs('ygoprodeck-01-03-25.json')
+    const imagesToDownload = await findImgs('ygoprodeck-02-17-25.json')
     try {
         await downloadImages(imagesToDownload);
         console.log('\nDownload Completed');
